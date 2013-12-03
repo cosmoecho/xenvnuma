4d95bd78235bcba8270c02caf12864e6fb2f119e
Elena Ufimtseva <ufimtseva@gmail.com>
2013-12-03 05:50:39 -0500

    xen: vNUMA support for PV guests
    Defines interface, structures and hypercalls for toolstack to
    build vnuma topology and for guests that wish to retreive it.
    Two subop hypercalls introduced by patch:
    XEN_DOMCTL_setvnumainfo to define vNUMA domain topology per domain
    and XENMEM_get_vnuma_info to retreive that topology by guest.
    
    Signed-off-by: Elena Ufimtseva <ufimtseva@gmail.com>

--- xen/include/public/memory.h
+++ xen/include/public/memory.h
@@ -339,6 +339,27 @@ struct xen_pod_target {
 };
 typedef struct xen_pod_target xen_pod_target_t;
 
+/*
+ * XENMEM_get_vnuma_info used by caller to retrieve
+ * vNUMA topology constructed for particular domain.
+ *
+ * The data exchanged is presented by vnuma_topology_info.
+ */
+#define XENMEM_get_vnuma_info               25
+
+/*
+ * XENMEM_get_vnuma_nodes used to retreive number of nodes
+ * and vcpus for allocating correct amount of memory for
+ * vnuma topology.
+ */
+#define XENMEM_get_vnodes_vcpus             26
+
+/*
+ * XENMEM_get_vnuma_pnode used by guest to determine
+ * the physical node of the specified vnode.
+ */
+#define XENMEM_get_vnuma_pnode              27
+
 #if defined(__XEN__) || defined(__XEN_TOOLS__)
 
 #ifndef uint64_aligned_t
